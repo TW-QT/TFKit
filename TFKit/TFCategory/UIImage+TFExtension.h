1e905f39e9245bddc8761987ed9d88642fc69b70
Donkey <taofei0610@gmail.com>
2017-03-17 13:30:27 +0800

    fix warning - -> + in UIImage

--- TFKit/TFCategory/UIImage+TFExtension.h
+++ TFKit/TFCategory/UIImage+TFExtension.h
@@ -11,6 +11,6 @@
 
 -(UIImage *)circleImage;
 
-- (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size;
++ (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size;
 
 @end
