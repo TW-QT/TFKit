8dcb66e92857958c3ea1b5e10786deaa6435c674
Donkey-Tao <taofei0610@gmail.com>
2017-04-08 15:18:18 +0800

    fix 8 -> 6

--- TFKit/TFCategory/NSString+TFExtension.m
+++ TFKit/TFCategory/NSString+TFExtension.m
@@ -88,7 +88,7 @@
     BOOL result = false;
     if ([self length] >= 6){
         // 判断长度大于8位后再接着判断是否同时包含数字和字符
-        NSString * regex = @"^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$";
+        NSString * regex = @"^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$";
         NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
         result = [pred evaluateWithObject:self];
     }
